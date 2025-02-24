1ad317d35c31042bf01d6fcaade8f3d659443dcc
Van Nhanh <trvannhanh@gmail.com>
2025-02-24 14:20:12 +0700

    ma

---
+++ KTPMPj/src/main/java/com/mycompany/ktpmpj/KTPMPj.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ktpmpj;
+
+/**
+ *
+ * @author admin
+ */
+public class KTPMPj {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
